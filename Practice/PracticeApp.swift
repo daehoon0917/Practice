56be7b685f271b0535d0bdea9e6cfa8f8947d015
daehoon0917 <daehoon0917@naver.com>
2023-09-18 11:10:24 +0900

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by 성대훈 on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
